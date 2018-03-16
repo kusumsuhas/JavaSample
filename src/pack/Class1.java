efc25332c19420d555d51f31d085b06e44f56356
kusumsuhas <ravindra.v3@gmail.com>
2018-03-16 17:06:57 +0530

    Updated sysout

--- src/pack/Class1.java
+++ src/pack/Class1.java
@@ -5,7 +5,7 @@ public class Class1 {
 	public static void main(String[] args) {
 		
 		for (int i =1; i<=10; i++) {
-			System.out.println("Number "+i);
+			System.out.println("Number Updated "+i);
 		}
 	}
 }
