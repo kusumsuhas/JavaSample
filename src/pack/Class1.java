cd807a1527a31497f7a5231c27091c28796b1daf
kusumsuhas <ravindra.v3@gmail.com>
2018-07-16 13:29:28 +0530

    Changed i value

--- src/pack/Class1.java
+++ src/pack/Class1.java
@@ -4,7 +4,7 @@ public class Class1 {
 
 	public static void main(String[] args) {
 		
-		for (int i =1; i<=10; i++) {
+		for (int i =1; i<=11; i++) {
 			System.out.println("Number Updated "+i);
 		}
 	}
